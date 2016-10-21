88c50fa724bb87e4a180948c7942a1b8737b02f6
leonardlee2015 <leonardlee2013@gmail.com>
2016-10-21 16:49:39 +0800

    Initial Commit

---
+++ MVVMDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MVVMDemo
+//
+//  Created by  Leonard on 2016/10/21.
+//  Copyright © 2016年  Leonard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MVVMDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MVVMDemo
+//
+//  Created by  Leonard on 2016/10/21.
+//  Copyright © 2016年  Leonard. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
